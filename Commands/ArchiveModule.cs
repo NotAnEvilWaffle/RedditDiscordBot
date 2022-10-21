ee8a4ba3ebc0d538edcca020b9ebbd64848c4b8a
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2022-10-21 14:05:47 -0500

    Add project files.

---
+++ Commands/ArchiveModule.cs
@@ -0,0 +1,115 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Dropbox.Api;
+using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading;
+
+namespace DiscordBotV2.Commands
+{
+    class ArchiveModule : BaseCommandModule
+    {
+
+        CancellationTokenSource tokenSource = null;
+        static string token = " ";
+
+        ulong C_HOF = 0;
+        ulong C_HOSD = 0;
+        ulong C_NFBC = 0;
+
+        [Command("archive")]
+        public async Task ArchiveCommand(CommandContext ctx, DiscordChannel channel, int num)
+        {
+            var Owner = ctx.Guild.Owner;
+            if (ctx.User == Owner)
+            {
+                DropboxClient dbx = new DropboxClient(token);
+                tokenSource = new CancellationTokenSource();
+                int timeout = 10000;
+
+                Console.WriteLine("Connecting to DropBox...\n");
+
+
+                var Messages = await channel.GetMessagesAsync(num);
+                Console.WriteLine($"Fetching Messages from {channel}\n");
+
+                using (WebClient client = new WebClient())
+                {
+
+                    
+
+                    var length = Messages.Count;
+                    var notiMSG = await channel.SendMessageAsync("Archiving is in Progress of this channel\nDo not use the bot until finished.");
+
+                    var HOF = await ctx.Client.GetChannelAsync(C_HOF);
+                    var HOSD = await ctx.Client.GetChannelAsync(C_HOSD);
+                    var NFBC = await ctx.Client.GetChannelAsync(C_NFBC);
+
+                    string folder = null;
+
+                    if(channel == HOF)
+                    {
+                        folder = "";
+                    }
+                    else if (channel == HOSD)
+                    {
+                        folder = "";
+                    }
+                    else if (channel == NFBC)
+                    {
+                        folder = "";
+                    }
+
+                    int failCtr = 0;
+
+                    foreach (var message in Messages)
+                    {
+                        if (message.Author == ctx.Client.CurrentUser)
+                        {
+                            Console.WriteLine("Found Message\n");
+
+                            var URL = message.Embeds[0].Image.Url.ToUri();
+
+                            Console.WriteLine(URL.ToString());
+                            //TODO: Catch exception from Stream
+                            try
+                            {
+                                using (Stream stream = client.OpenRead(URL))
+                                {
+                                    string uriSeg = URL.Segments[1];
+
+                                    var response = await dbx.Files.UploadAsync(path: folder + uriSeg, body: stream);
+
+                                    Console.WriteLine("Archiving: " + URL);
+
+
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Could not upload this image");
+                                failCtr++;
+                                continue;
+                                throw;
+                            }
+                            
+                        }
+                    }
+
+                    var finMSG = await channel.SendMessageAsync($"Completed\n{failCtr} images failed to upload\nYou may resume normal bot usage");
+
+                    await Task.Delay(3000);
+
+                    await notiMSG.DeleteAsync();
+                    await finMSG.DeleteAsync();
+                }
+            }
+            
+
+        }
+
+    }
+}
---
+++ Commands/SendModule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using DSharpPlus.CommandsNext;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Reddit;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace DiscordBotV2.Commands
+{
+    public class SendModule : BaseCommandModule
+    {
+
+        ulong botChannel = 0;
+        String subreddit = "mildlyinteresting";
+
+        [Command("send"), Cooldown(1,7,CooldownBucketType.User)]
+        public async Task Send(CommandContext ctx)
+        {
+            if (ctx.Message.ChannelId == botChannel)
+            {
+               
+                var currentSubreddit = Program.reddit.Subreddit(subreddit).About();
+                List<Reddit.Controllers.Post> topPosts = currentSubreddit.Posts.Hot;
+
+                int r = Program.random.Next(0, 99);
+
+                string permalink = topPosts[r].Permalink;
+                string jsonLink = "https://www.reddit.com" + permalink + ".json";
+
+                string link;
+
+                using (WebClient wc = new WebClient())
+                {
+                    var json = wc.DownloadString(jsonLink);
+                    //JArray obj = JArray.Parse(json);
+
+                    dynamic res = JArray.Parse(json);
+
+                    List<Rootobject> obj = JsonConvert.DeserializeObject<List<Rootobject>>(json);
+                    // Console.WriteLine(res.data.children[0].data.url_overridden_by_dest);
+                    link = obj[0].data.children[0].data.url_overridden_by_dest;
+
+                }
+
+                DiscordEmoji thumbUp = DiscordEmoji.FromName(ctx.Client,":thumbsup:");
+                DiscordEmoji okHand = DiscordEmoji.FromName(ctx.Client, ":ok_hand:");
+                DiscordEmoji noSign = DiscordEmoji.FromName(ctx.Client, ":no_entry_sign:");
+                DiscordEmoji cuteHeart = DiscordEmoji.FromName(ctx.Client, ":cuteheart:");
+
+                var message = await ctx.Message.RespondAsync(embed: new DiscordEmbedBuilder
+                {
+                    ImageUrl = link,
+                    Color = DiscordColor.Black
+
+
+                }.Build());
+
+                await message.CreateReactionAsync(thumbUp);
+                await message.CreateReactionAsync(okHand);
+                await message.CreateReactionAsync(noSign);
+                await message.CreateReactionAsync(cuteHeart);
+            }
+        }
+
+
+
+    }
+}
